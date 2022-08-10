be52d4cf854fe572fa8b57ef4cb2fc5ae0746a0f
anilt <anilt@Appi174>
2022-08-10 16:12:57 +0530

    added anagram

---
+++ Java8test/src/Anagram.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Anagram {
+	
+	public static void main(String[] args) {
+		String s=" asssssa";
+		String s1="asssssa";
+		 int res = Integer.MAX_VALUE;
+		Arrays.sort(s.toCharArray());
+		System.out.println(res);
+		
+		
+		
+		
+		s.equalsIgnoreCase(s1);
+	}
+
+}
